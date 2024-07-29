f285663a4c51373b7575be91b6a9231f76739d61
Alex Yuwen <ayuwen@osisoft.com>
2024-07-28 19:56:27 -0700

    .

--- sketch.js
+++ sketch.js
@@ -1,8 +1,8 @@
 let canvasWidth;
 let canvasHeight;
 let centerX, centerY;
-let diameter;
-let offsets = {'centerX': 0, 'centerY': 1, 'diameter': 2};
+let r;
+let offsets = {'centerX': 0, 'centerY': 1, 'r': 2};
 let emily;
 let emilyWidth;
 let emilyHeight;
@@ -17,8 +17,8 @@ function setup() {
   canvasHeight = windowHeight;
   createCanvas(canvasWidth, canvasHeight);
 
-  emilyWidth = emily.width / 1.5;
-  emilyHeight = emily.height / 1.5;
+  emilyWidth = (emily.width / emily.height) * canvasHeight;
+  emilyHeight = canvasHeight;
 
   angleMode(DEGREES);
 }
@@ -26,16 +26,14 @@ function setup() {
 function draw() {
   centerX = map(noise(offsets.centerX), 0, 1, 0, canvasWidth / 3);
   centerY = map(noise(offsets.centerY), 0, 1, 0, canvasHeight / 2);
-  diameter = map(noise(offsets.diameter), 0, 1, 0, canvasWidth);
+  r = map(noise(offsets.r), 0, 1, 0, 2);
   
   background(dancing);
-  // circle(centerX, centerY, diameter);
-  r = noise(offsets.diameter);
   image(emily, centerX, centerY, r * emilyWidth, r * emilyHeight);
   
   offsets.centerX += 0.01;
   offsets.centerY += 0.01;
-  offsets.diameter += 0.01;
+  offsets.r += 0.01;
 }
 
 // Dynamic window resizing
